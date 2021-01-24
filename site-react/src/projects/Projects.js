53c58405aebb5da75fd51e11e686e6754e7257b7
Hari <charanp@gmail.com>
2021-01-24 00:00:34 -0500

    added personal projects

--- site-react/src/projects/Projects.js
+++ site-react/src/projects/Projects.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import LaunchIcon from '@material-ui/icons/Launch';
-import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
+import React, { useContext, useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';  
+import Grid from '@material-ui/core/Grid';  
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader'; 
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions'; 
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography'; 
+import LaunchIcon from '@material-ui/icons/Launch';
+import ProfileContext from '../context/ProfileContext';
+
+const useStyles= makeStyles( (theme)=> ({
   root: {
     margin: 10,
     display: 'flex',
@@ -26,53 +20,72 @@ const useStyles = makeStyles((theme) => ({
     alignContent: 'center',
     flexWrap: 'wrap',
     listStyle: 'none',
-    padding: theme.spacing(0.5),
-    fontFamily: "monospace"  
+    padding: theme.spacing(0.5)  
   },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
+  skillItem: {
+    alignSelf: 'center'
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+  skillContainer: {
+    display: 'flex',
+    margin: '0.8px', 
+    paddingRight: 24, 
+    paddingLeft: 24,
+    backgroundColor: '#e0e0e0',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap', 
+    textOverflow: 'ellipsis',
+    cursor: 'pointer',
+    fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+    height: 64, 
+    maxWidth:345
   },
-  expandOpen: {
-    transform: 'rotate(180deg)',
+  low : {
+    fontSize: 15, 
   },
-  avatar: {
-    backgroundColor: red[500],
+  med : {
+    fontSize: 25, 
   },
-  contactHeader: {
+  high : {
+    fontSize: 45, 
+  },
+    contactHeader: {
     fontSize: 20,
     borderBottom: 2,
     backgroundColor: 'antiquewhite'
   },
-  card: {
-      cursor: 'pointer',
-      maxWidth: 345
+    card: {
+      marginRight: 5,
+      marginBottom: 5
   }
 }));
 
-export default function Projects() {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+const Projects = (props) =>{
+  const data = useContext(ProfileContext);
+  const classes = useStyles(); 
+  const [selectedSkill, setSelectedSkill] = useState('');  
+  const [open, setOpen] = useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  useEffect(() => {
+    document.title="Personal Projects";
+  }, []);
+
+  const handleSkillClick = (item)=>{ 
+    setOpen(true);
   };
 
-  const getProjectCard = () =>{
-      return (
-        <Grid item xs={12}  sm={6} > 
-            <div onClick={()=>window.open('https://g-math.netlify.app')} className={classes.card}>
-                <Card  >
-                <CardHeader
+  const handleClose = () => { 
+    setOpen(false);
+  }
+
+  const getSkillChip = (item) => { 
+    const chipClass= `${classes.skillContainer} ${classes['high']}`;
+    return ( 
+      <li>  
+              <div onClick={()=>window.open(item.url)} className={classes.card}>
+                 <Card  >
+                 <CardHeader
                     className={classes.contactHeader}
-                    title="gMath" 
+                    title={item.title} 
                     action={
                         <IconButton aria-label="launch-app">
                         <LaunchIcon />
@@ -81,7 +94,7 @@ export default function Projects() {
                 /> 
                 <CardContent>
                     <Typography variant="body2" color="textSecondary" component="p">
-                    tiny math app created for my kid using React and Redux
+                    {item.description}
                     </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
@@ -90,24 +103,34 @@ export default function Projects() {
                     </IconButton>   */}
                 </CardActions> 
                 </Card>
-            </div>
-        </Grid>
-      );
+            </div> 
+      </li> 
+    );
+  } 
+  
+  if (!data){
+    return <div>Loading...</div>
   }
 
-  return (
-    
-    <Grid container 
-        direction="row" 
-        wrap="nowrap"
-        className={classes.root}
-        justify="center"
-        alignItems="center"
-    >
-        {getProjectCard()}
+  return(  
+    <Grid container direction="row"
+    justify="center"
+    alignItems="center"> 
+      <Grid item xs={12} sm={8}>  
 
-        <div>...</div>
-    </Grid>       
+        <Paper component="ul" className={classes.root} elevation={0}>
+            {data.projects.map((data) => { 
+              return (
+                <React.Fragment key={data.id}>
+                  {getSkillChip(data)} 
+                </React.Fragment>
+              );
+            })}
+        </Paper> 
 
-  );
-}
+      </Grid> 
+    </Grid> 
+  )
+};
+
+export default Projects;
