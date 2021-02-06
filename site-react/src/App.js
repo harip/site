d68ae8e4f1036a38d5832de30ccff854cd0a4fde
harip <charanp@gmail.com>
2021-02-05 22:08:59 -0500

    add ability to add posts

--- site-react/src/App.js
+++ site-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { HashRouter, Route  } from 'react-router-dom';  
 import NavBar from './common/components/navbar/NavBar';
 import { ProfileProvider } from './context/ProfileContext'; 
@@ -8,24 +8,30 @@ import Resume from './resume/Resume';
 import SkillCloud from './skills/SkillCloud';
 import FloatingActions from './common/components/floatingactions/FloatingActions'; 
 
+const AddPostLazy = lazy(()=>import('./admin/addpost/AddPost'));
+
 const App = () => {
   return (
-    <HashRouter  >
-      <NavBar />  
-      <FloatingActions/> 
-      <Route path="/" exact component={SkillCloud} />
+    <React.Fragment>
+      <HashRouter  > 
+          <NavBar />  
+          <FloatingActions/> 
+          <Route path="/" exact component={SkillCloud} />
           <Route path="/resume" exact component={Resume} /> 
           <Route path="/projects" exact component={Projects} />
-          <Route path="/pythonds" exact component={PythonDs} />       
- 
-    </HashRouter>
+          <Route path="/pythonds" exact component={PythonDs} />     
+          <Route path="/postarticle" exact component={AddPostLazy} />  
+      </HashRouter> 
+    </React.Fragment>
   )
 };
 
 export const profileApp = () => {
   return (
     <ProfileProvider>
+      <Suspense fallback={<div>Loading...</div>}>
       <App/>
+      </Suspense>
     </ProfileProvider>
   );
 };
