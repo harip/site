c128d1982a2b4ac5da233290a9cb78a0542a9d00
Hari <charanp@gmail.com>
2024-01-15 00:46:16 -0500

    images

--- sire-react-v2/src/components/Distractions/Distractions.tsx
+++ sire-react-v2/src/components/Distractions/Distractions.tsx
@@ -3,11 +3,30 @@ import './Distractions.css';
 import { Grid, Dialog, IconButton } from '@mui/material'; 
 import CloseIcon from '@mui/icons-material/Close';
 import ImageComponent from './ImageComponent';
-import Image1 from './images/logo512.png'; 
+import Image1 from './images/CamScanner 12-04-2023 22.30_1.jpg';
+import Image2 from './images/CamScanner 12-04-2023 22.30_2.jpg';
+import Image3 from './images/CamScanner 12-04-2023 22.30_3.jpg';
+import Image4 from './images/CamScanner 12-04-2023 22.30_4.jpg';
+import Image5 from './images/CamScanner 12-04-2023 22.30_5.jpg';
+import Image6 from './images/CamScanner 12-04-2023 22.30_6.jpg';
+import Image7 from './images/CamScanner 12-04-2023 22.30_7.jpg';
+import Image8 from './images/CamScanner 12-04-2023 22.30_8.jpg';
+import Image9 from './images/CamScanner 12-04-2023 22.30_9.jpg';
+import Image10 from './images/CamScanner 12-04-2023 22.30_10.jpg';
+import Image11 from './images/CamScanner 12-04-2023 22.30_11.jpg';
+import Image12 from './images/CamScanner 12-04-2023 22.30_12.jpg';
+import Image13 from './images/CamScanner 12-04-2023 22.30_13.jpg';
+import Image14 from './images/CamScanner 12-04-2023 22.30_14.jpg';
+import Image15 from './images/CamScanner 12-04-2023 22.30_15.jpg';
+import Image16 from './images/CamScanner 12-04-2023 22.30_16.jpg';
+import Image17 from './images/CamScanner 12-04-2023 22.30_17.jpg';
+import Image18 from './images/CamScanner 12-04-2023 22.30_18.jpg';
+import Image19 from './images/CamScanner 12-04-2023 22.30_19.jpg';
+import Image20 from './images/CamScanner 12-04-2023 22.30_20.jpg';
 
 const Distractions: React.FC = () => {
 
-  const images = [Image1];
+  const images = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12, Image13, Image14, Image15, Image16, Image17, Image18, Image19, Image20];
   const [selectedImage, setSelectedImage] = useState("");
 
   const handleClose = () => {
